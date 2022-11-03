926fe91f6bc8bf89b13d45516bafda11fe3bceb1
Sandesh Thapa <sandeshthapa@Apples-Mac-mini.local>
2022-11-03 17:33:02 +0545

    update: footer ongoing

--- footer.php
+++ footer.php
@@ -1,8 +1,46 @@
 <footer class="footer">
     <div class="container">
-        
+        <div class="footer__top">
+            <div class="footer__top-left">
+                <h2 class="footer__title">Contact Us, Head Office</h2>
+                <p>Thamel, Kathmandu, Nepal <br>GPO Box 5148</p>
+                <a href="tel:+97714544302">+977 1 4544302</a>
+                <div class="footer__top-content">
+                    <p>Inquire us @</p>
+                    <a href="mailto: info@travelmaxguide.com">info@travelmaxguide.com</a>
+                </div>
+                <div class="footer__top-content">
+                    <p>Call us @</p>
+                    <a href="tel:+447473707706">+44 7473 707706</a>
+                </div>
+            </div>
+            <div class="footer__top-right">
+                <h2 class="footer__title">Subscribe to our Newsletter</h2>
+                <p>Sign up to receive our trip ideas and travel offers!</p>
+                <form action="/action_page.php">
+                    <input type="email" id="email" name="email" placeholder="hello@example.com">
+                    <input type="submit" value="Subscribe">
+                </form>
+            </div>
+        </div>
+        <div class="footer__medium">
+            <div class="footer__medium-left">
+                <h3 class="footer__subtitile">USA</h3>
+                <p>44479 cayuga dr St.louis, MO 63123</p>
+                <a href="tel:+3144897247">+314 897247</a>
+                <a href="mailto: suru@travelmaxguide.com">suru@travelmaxguide.com</a>
+            </div>
+            <div class="footer__medium-left">
+                <h3 class="footer__subtitile">UK</h3>
+                <p>1C Aldeburgh Street, Se10 Orw Greenwich, London</p>
+                <a href="tel:+3144897247">+314 897247</a>
+                <a href="mailto: jason@travelmaxguide.com">jason@travelmaxguide.com</a>
+            </div>
+        </div>
     </div>
 </footer>
 <?php wp_footer(); ?>
+
 </body>
+
 </html>
