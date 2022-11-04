8fd82f1fd58ebcda53861dbad9395bb790286e60
Sandesh Thapa <sandeshthapa@Apples-Mac-mini.local>
2022-11-04 16:44:54 +0545

    added: footer with responsive

--- footer.php
+++ footer.php
@@ -3,12 +3,14 @@
         <div class="footer__top">
             <div class="footer__top-left">
                 <h2 class="footer__title">Contact Us, Head Office</h2>
-                <p>Thamel, Kathmandu, Nepal <br>GPO Box 5148</p>
-                <a href="tel:+97714544302">+977 1 4544302</a>
+                <p>Thamel, Kathmandu, Nepal <br>GPO Box 5148<br>+977 1 4544302</p>
+
                 <div class="footer__top-content">
                     <p>Inquire us @</p>
                     <a href="mailto: info@travelmaxguide.com">info@travelmaxguide.com</a>
+
                 </div>
+                <hr class="footer__line">
                 <div class="footer__top-content">
                     <p>Call us @</p>
                     <a href="tel:+447473707706">+44 7473 707706</a>
@@ -17,9 +19,9 @@
             <div class="footer__top-right">
                 <h2 class="footer__title">Subscribe to our Newsletter</h2>
                 <p>Sign up to receive our trip ideas and travel offers!</p>
-                <form action="/action_page.php">
+                <form action="/action_page.php" class="footer__form">
                     <input type="email" id="email" name="email" placeholder="hello@example.com">
-                    <input type="submit" value="Subscribe">
+                    <input type="submit" value="Subscribe" id="subscribe">
                 </form>
             </div>
         </div>
@@ -37,10 +39,151 @@
                 <a href="mailto: jason@travelmaxguide.com">jason@travelmaxguide.com</a>
             </div>
         </div>
+        <! list of logo of company !>
+            <div class="footer__media">
+                <div class="footer__media-left">
+                    <h3 class="footer__subtitile">We are with</h3>
+                    <div class="footer__media-grid">
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer1.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer2.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer3.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer2.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                    </div>
+                </div>
+                <div class="footer__media-right">
+                    <h3 class="footer__subtitile">Recommended by</h3>
+                    <div class="footer__media-grid ">
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer6.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer4.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer5.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                        <figure class="footer__media-list">
+                            <img class="footer__media-image"
+                                src="<?php echo get_template_directory_uri() ?>/uploads/footer7.png"
+                                alt="Image of a company logo" />
+                        </figure>
+                    </div>
+                </div>
+            </div>
+
+            <! list of social media !>
+                <div class="footer__icons">
+                    <div class="footer__iconlist">
+                        <a href="#" target="_blank"><svg class="icon icon-facebook">
+                                <use xlink:href="#icon-facebook"></use>
+                            </svg>
+                        </a>
+                        <a href="#" target="_blank"><svg class="icon icon-twitter">
+                                <use xlink:href="#icon-twitter"></use>
+                            </svg>
+                        </a>
+                        <a href="#" target="_blank"><svg class="icon icon-instagram">
+                                <use xlink:href="#icon-instagram"></use>
+                            </svg></a>
+                        <a href="#" target="_blank"><svg class="icon icon-youtube">
+                                <use xlink:href="#icon-youtube"></use>
+                            </svg></a>
+                        <a href="#" target="_blank"> <svg class="icon icon-linkedin">
+                                <use xlink:href="#icon-linkedin"></use>
+                            </svg></a>
+                    </div>
+                </div>
+
+                <! Footer nav links !>
+
+                    <div class="footer__nav">
+                        <ul class="footer__lists">
+                            <li class="footer__links">
+                                <a href="#">Partner With Us</a>
+                            </li>
+                            <li class="footer__links">
+                                <a href="#">SiteMap</a>
+                            </li>
+                            <li class="footer__links">
+                                <a href="#">Privacy Policy</a>
+                            </li>
+                            <li class="footer__links">
+                                <a href="#">Terms & Conditions</a>
+                            </li>
+                            <li class="footer__links">
+                                <a href="#">Responsible Travel</a>
+                            </li>
+                        </ul>
+
+                    </div>
+                    <! sponsors list !>
+                        
+                            <figure class="footer__sponsors">
+                                <img class="footer__media-image"
+                                    src="<?php echo get_template_directory_uri() ?>/uploads/sponsor.png"
+                                    alt="Image of a company logo" />
+                            </figure>
+                        
+                        <div class="footer__bottom">
+                            <p>© 2009 - 2020<br>All rights reserved. Travel Max Guide</p>
+                            <h4 class="footer__bottom-title">Crafted by last door</h4>
+                        </div>
+
     </div>
 </footer>
 <?php wp_footer(); ?>
+<svg style="position:absolute;width:0;height:0" overflow="hidden">
+    <defs>
+        <symbol id="icon-facebook" viewBox="0 0 32 32">
+            <path
+                d="M16 0.524c-8.547 0-15.476 6.929-15.476 15.476 0 7.759 5.715 14.167 13.163 15.286v-11.184h-3.829v-4.069h3.829v-2.706c0-4.481 2.183-6.448 5.908-6.448 1.783 0 2.727 0.133 3.174 0.191v3.553h-2.541c-1.581 0-2.132 1.5-2.132 3.188v2.223h4.634l-0.628 4.069h-4.006v11.217c7.554-1.025 13.381-7.482 13.381-15.318 0-8.547-6.929-15.476-15.476-15.476z">
+            </path>
+        </symbol>
+        <symbol id="icon-twitter" viewBox="0 0 40 32">
+            <path
+                d="M39.252 4.377c-1.42 0.628-2.945 1.048-4.553 1.246 1.636-0.971 2.893-2.518 3.484-4.35-1.531 0.897-3.225 1.557-5.031 1.903-1.438-1.526-3.494-2.473-5.77-2.473-4.373 0-7.918 3.513-7.918 7.844 0 0.615 0.071 1.212 0.206 1.792-6.577-0.333-12.41-3.458-16.314-8.206-0.686 1.161-1.072 2.518-1.072 3.951 0 2.724 1.394 5.123 3.521 6.535-1.299-0.040-2.521-0.401-3.59-0.979 0 0.024 0 0.058 0 0.090 0 3.806 2.729 6.976 6.348 7.699-0.662 0.179-1.359 0.28-2.080 0.28-0.512 0-1.011-0.058-1.494-0.145 1.008 3.112 3.933 5.384 7.396 5.453-2.711 2.101-6.123 3.357-9.834 3.357-0.641 0-1.267-0.037-1.887-0.111 3.505 2.22 7.667 3.521 12.136 3.521 14.561 0 22.524-11.956 22.524-22.329 0-0.34-0.011-0.678-0.026-1.014 1.555-1.098 2.895-2.481 3.954-4.065z">
+            </path>
+        </symbol>
+        <symbol id="icon-instagram" viewBox="0 0 32 32">
+            <path
+                d="M9.486 0.375c-5.022 0-9.111 4.092-9.111 9.116v13.020c0 5.024 4.092 9.114 9.116 9.114h13.020c5.024 0 9.114-4.092 9.114-9.116v-13.022c0-5.022-4.092-9.111-9.116-9.111h-13.022zM25.375 5.375c0.691 0 1.25 0.559 1.25 1.25s-0.559 1.25-1.25 1.25c-0.691 0-1.25-0.559-1.25-1.25s0.559-1.25 1.25-1.25zM16 7.875c4.482 0 8.125 3.643 8.125 8.125s-3.645 8.125-8.125 8.125c-4.482 0-8.125-3.645-8.125-8.125s3.643-8.125 8.125-8.125zM16 10.375c-3.108 0-5.625 2.517-5.625 5.625s2.517 5.625 5.625 5.625c3.108 0 5.625-2.517 5.625-5.625s-2.517-5.625-5.625-5.625z">
+            </path>
+        </symbol>
+        <symbol id="icon-youtube" viewBox="0 0 42 32">
+            <path
+                d="M40.107 4.626c-0.266-1.123-1.111-2.026-2.24-2.383-2.991-0.949-9.482-1.868-16.867-1.868s-13.876 0.919-16.867 1.868c-1.129 0.357-1.974 1.26-2.24 2.383-0.5 2.124-1.205 6.064-1.205 11.374s0.705 9.25 1.205 11.374c0.265 1.123 1.111 2.026 2.24 2.383 2.991 0.949 9.482 1.868 16.867 1.868s13.876-0.919 16.867-1.868c1.129-0.357 1.974-1.26 2.24-2.383 0.5-2.124 1.205-6.064 1.205-11.374s-0.705-9.25-1.205-11.374zM17.094 22.25v-12.5l11.606 6.25-11.606 6.25z">
+            </path>
+        </symbol>
+        <symbol id="icon-linkedin" viewBox="0 0 32 32">
+            <path
+                d="M26.526 0.211h-21.053c-2.907 0-5.263 2.357-5.263 5.263v21.053c0 2.907 2.357 5.263 5.263 5.263h21.053c2.907 0 5.263-2.357 5.263-5.263v-21.053c0-2.907-2.357-5.263-5.263-5.263zM10.737 25.868h-3.289v-13.158h3.289v13.158zM10.755 10.084c-0.424 0.434-0.979 0.653-1.663 0.653-0.676 0-1.226-0.224-1.658-0.673-0.429-0.447-0.645-1-0.645-1.65 0-0.671 0.216-1.218 0.645-1.645 0.432-0.424 0.982-0.637 1.658-0.637 0.684 0 1.239 0.213 1.663 0.637 0.427 0.424 0.64 0.974 0.64 1.645 0 0.681-0.213 1.236-0.64 1.67zM25.868 25.868h-3.29v-7.311c0-2.038-1.174-3.215-2.632-3.215-0.702 0-1.496 0.427-1.951 0.966s-0.681 1.226-0.681 2.056v7.504h-3.29v-13.158h3.29v2.082h0.051c0.969-1.601 2.377-2.398 4.228-2.403 2.629-0.008 4.274 1.966 4.274 5.422v8.057z">
+            </path>
+        </symbol>
 
+    </defs>
+</svg>
 </body>
 
 </html>
--- front-page.php
+++ front-page.php
@@ -3,7 +3,7 @@ get_header();
 the_post();
 ?>
 <main class="page__home">
-   Body
+  
     <?php include('modules/flex-content/flex-content.php') ?>
 </main>
 <?php get_footer(); ?>
